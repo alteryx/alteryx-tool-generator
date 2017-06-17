7472321877ee8de75c0cad379d3baf1616119981
Unknown <odembowski@alteryx.com>
2017-06-16 18:34:05 -0700

    US26161: Tool Folder
    US26161: Tool Folder

--- alteryx-tool-generator.js
+++ alteryx-tool-generator.js
@@ -7,6 +7,7 @@ const argv = require('minimist')(process.argv.slice(2))
 const program = require('commander')
 const prompt = require('prompt')
 const colors = require('colors')
+const os = require('os')
 
 program
   .version('v1.0.0')
@@ -29,93 +30,103 @@ const schema = {
 	//     before: function(value) { return 'v' + value; } // Runs before node-prompt callbacks. It modifies user's input
 	// },
     ToolName: {
-	  	description: 'Tool Name',
-	    pattern: /^[a-zA-Z\s\-]+$/,
-	    message: 'Tool Name must be only letters, spaces, or dashes',
-	    required: true
+      description: 'Tool Name',
+      pattern: /^[a-zA-Z\s\-]+$/,
+      message: 'Tool Name must be only letters, spaces, or dashes',
+      required: true
     },
     NameIsFileName: {
-    	description: 'Is the tool name the same as the file name? (T/F)',
-    	type: 'string',
-    	required: true
+      description: 'Is the tool name the same as the file name? (T/F)',
+      type: 'string',
+      required: true
     },
     RootToolName: {
-    	description: 'Root Tool Name',
-    	type: 'string',
-    	required: true
+      description: 'Root Tool Name',
+      type: 'string',
+      required: true
     },
     Version: {
-    	description: 'Tool Version',
-    	type: 'number',
-    	required: false,
-    	default: 1
+      description: 'Tool Version',
+      type: 'number',
+      required: false,
+      default: 1
     },
     Author: {
-	  	description: 'Author',
-	    pattern: /^[a-zA-Z\s\-]+$/,
-	    message: 'Author must be only letters, spaces, or dashes',
-	    required: false
+      description: 'Author',
+      pattern: /^[a-zA-Z\s\-]+$/,
+      message: 'Author must be only letters, spaces, or dashes',
+      required: false
     },
     Company: {
-    	description: 'Company',
-    	type: 'string',
-    	required: false
+      description: 'Company',
+      type: 'string',
+      required: false
     },
     Copyright: {
-    	description: 'Copyright',
-    	type: 'string',
-    	required: false
+      description: 'Copyright',
+      type: 'string',
+      required: false
     },
     Category: {
-    	description: 'Category',
-    	type: 'string',
-    	required: false
+      description: 'Category',
+      type: 'string',
+      required: false
     },
     SearchTags: {
-    	description: 'Search Tags - separate by commas',
-    	type: 'string',
-    	required: false
+      description: 'Search Tags - separate by commas',
+      type: 'string',
+      required: false
     },
     Description: {
-    	description: 'Description',
-    	type: 'string',
-    	required: false
+      description: 'Description',
+      type: 'string',
+      required: false
     },
     DescriptionLink: {
-    	description: 'Description Link',
-    	type: 'string',
-    	required: false
+      description: 'Description Link',
+      type: 'string',
+      required: false
     },
     Backend: {
-    	description: 'Will the engine for your tool be a macro (M) or JavaScript (J) file?',
-    	type: 'string',
-    	required: true
+      description: 'Will the engine for your tool be a macro (M) or JavaScript (J) file?',
+      type: 'string',
+      required: true
     },
     IconPath: {
-    	description: 'Path to Icon image file',
-    	type: 'string',
-    	required: false,
-    	default: 'default_icon.png'
+      description: 'Path to Icon image file',
+      type: 'string',
+      required: false,
+      default: 'default_icon.png'
     },
     InputConnections: {
-    	description: 'Number of input connections',
-    	type: 'number',
-    	required: true
+      description: 'Number of input connections',
+      type: 'number',
+      required: true
     },
     OutputConnections: {
-    	description: 'Number of output connections',
-    	type: 'number',
-    	required: true
+      description: 'Number of output connections',
+      type: 'number',
+      required: true
     }
   }
 }
+
+let toolDirectory = ''
 
 console.log('\nEnter the following to configure your project...\n')
 prompt.message = '' // removes prompt: from the front of each question
 prompt.start()
 
 prompt.get(schema, function (err, result) {
-  userInput = result
+  const userInput = result
+  const folderName = userInput.ToolName + '_v' + userInput.Version
+  toolDirectory = 'C:\\Users\\' + os.userInfo().username +  '\\AppData\\Roaming\\Alteryx\\Tools\\' + folderName
+  fs.mkdir(toolDirectory, function(err) {
+    if (err) {
+      return console.error(err)
+    }
+    console.log(folderName + ' folder has been created')
+  })
   console.log('\nUser Inputs:\n')
   console.log(JSON.stringify(userInput, null, 4))
 })
@@ -127,12 +138,12 @@ const inputSchema = {
     InputConnectionLabel: {
     	description: '',
     	type: 'string',
-    	required:
+    	required: false
     },
     InputConnectionName: {
     	description: '',
     	type: 'string',
-    	required:
+    	required: false
     },
   }
 }
@@ -144,12 +155,12 @@ const outputSchema = {
     OutputConnectionLabel: {
     	description: '',
     	type: 'string',
-    	required:
+    	required: false
     },
     OutputConnectionName: {
     	description: '',
     	type: 'string',
-    	required:
+    	required: false
     },
   }
 }
