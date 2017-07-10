dfbf99eba3490835b12c05d63685f9442d3dd166
rithison87 <rithison@gmail.com>
2017-07-10 12:35:57 -0600

    US26162: backend logic updated with correct module, updated top level js file

--- app/create-engine-file.js
+++ app/create-engine-file.js
@@ -1,14 +1,9 @@
 const engineHtml = require('./create-engine-html.js')
-// placeholder to import create-engine-yxmc.js. MUST UPDATE
-// const engineYxmc = require('./create-engine-yxmc.js')
+const engineYxmc = require('./create-engine-yxmc.js')
 
 exports.creatEngineFile = (result) => new Promise((resolve, reject) => {
   if (result === null) reject(console.error('createEngineFile: input undefined'))
   const { Backend } = result
   if (Backend.toLowerCase() === 'j') resolve(engineHtml.createEngineHTML(result))
-  if (Backend.toLowerCase() === 'm') {
-    // update this section when create-engine-yxmc.js is created
-    console.log('Engine YXMC Placeholder')
-    resolve(result)
-  }
+  if (Backend.toLowerCase() === 'm') resolve(engineYxmc.createEngineYxmc(result))
 })
